96a5f2f22147dbd60161b9ce3101af7dde59d9f2
Kristóf Kádár <43924835+Kkrisi@users.noreply.github.com>
2025-02-18 11:56:08 +0100

    AuthContext.js korrigálás (useEffect import)

--- spenot/src/contexts/AuthContext.js
+++ spenot/src/contexts/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { myAxios } from "../api/axios";
 import { useNavigate } from "react-router-dom";
 
