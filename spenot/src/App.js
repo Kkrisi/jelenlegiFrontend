c2a712d963b7394f7351b940724704e73a407f3d
logan114 <kepirobalazs204@gmail.com>
2025-02-18 14:42:00 +0100

    app.JS vissza, nem lett jó

--- spenot/src/App.js
+++ spenot/src/App.js
@@ -1,8 +1,9 @@
+
 import './App.css';
 import Login from './pages/Login';
 import ForgotPassword from './pages/ForgotPassword';
 import Register from './pages/Register';
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EmailSend from './pages/EmailSend';
 import UploadDatabase from './pages/UploadDatabase';
 import ResetPassword from './pages/ResetPassword';
@@ -12,40 +13,42 @@ import Logs from './pages/Logs';
 import AdminPage from './pages/AdminPage';
 import Layout from './layouts/Layout';
 import ProtectedRoute from './components/ProtectedRoute';
-import useAuthContext from './contexts/AuthContext'; // Import the custom hook
+
+
+
+
 
 export default function App() {
-  const { user } = useAuthContext(); // Use the custom hook to get the user object
-  const userRole = user ? user.role : null; // Assuming `role` is a property of the user object
-
   return (
-    <Routes>
+        <Routes>
 
-      {/* mindenki számára elérhető */}
-      <Route path="/" element={<Login />} />
-      <Route path="/bejelentkezes" element={<Login />} />
-      <Route path="/regisztracio" element={<Register />} />
-      <Route path="/elfelejtJelszo" element={<ForgotPassword />} />
-      <Route path="/password-reset/:token" element={<ResetPassword />} />
+            {/* mindenki számára elérhető */}
+            <Route path="/" element={<Login />} />
+            <Route path="/bejelentkezes" element={<Login />} />
+            <Route path="/regisztracio" element={<Register />} />
+            <Route path="/elfelejtJelszo" element={<ForgotPassword />} />
+            <Route path="/password-reset/:token" element={<ResetPassword />} />
 
-      {/* bejelentkezett felhasználók */}
-      <Route element={<ProtectedRoute userRole={userRole}/>}>
-        <Route element={<Layout />}>
-          <Route path="/emailKuldes" element={<EmailSend />} />
-          <Route path="/naplozas" element={<Logs />} />
-          <Route path="/diakKezeles" element={<StudentsManagement />} />
-          <Route path="/abFeltoltes" element={<UploadDatabase />} />
-        </Route>
-      </Route>
 
-      {/* csak az admin */}
-      <Route element={<ProtectedRoute userRole={userRole}/>}>
-        <Route element={<Layout />}>
-          <Route path="/admin" element={<AdminPage />} />
-          <Route path="/felhKezeles" element={<UsersManagement />} />
-        </Route>
-      </Route>
+            {/* bejelentkezett felhasználók */}
+            <Route element={<ProtectedRoute />}>
+                <Route element={<Layout />}>
+                    <Route path="/emailKuldes" element={<EmailSend />} />
+                    <Route path="/naplozas" element={<Logs />} />
+                    <Route path="/diakKezeles" element={<StudentsManagement />} />
+                    <Route path="/abFeltoltes" element={<UploadDatabase />} />
+                </Route>
+            </Route>
 
-    </Routes>
+
+            {/* csak az admin */}
+            <Route element={<ProtectedRoute roleRequired={2} />}>
+                <Route element={<Layout />}>
+                    <Route path="/admin" element={<AdminPage />} />
+                    <Route path="/felhKezeles" element={<UsersManagement />} />
+                </Route>
+            </Route>
+
+        </Routes>
   );
 }
